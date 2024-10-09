612616df8abf3c0f04061da0705fdebd107a2ad5
jacob-kasaso <jacobkasaso@gmail.com>
2024-10-09 22:12:21 +0300

    scope practise

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+console.log('bob')
+
+
+function upperCaseCustomerName(){
+    customerName = 'BOB';
+    console.log('BOB');
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    console.log('not bob');
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    console.log('maybe bob')
+}
+
+const leastFavoriteCustomer = 'bob'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'BOB'
+    console.log('BOB')
+}
+
+
+
+
+
+
+
